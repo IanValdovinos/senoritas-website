ea9864505a3c32191ae3ae9096831629108defe9
Ian Samuel Valdovinos Granados <ian.valdovinos752001@gmail.com>
2024-03-20 21:16:56 -0400

    Add background image to seasonals and specials for mobile layout

--- src/components/DeliveryAvailable/index.js
+++ src/components/DeliveryAvailable/index.js
@@ -15,9 +15,9 @@ function DeliveryAvailable() {
              <div className={styles.deliveryAvailableSection}>
                 <HeadingOne>Delivery Available</HeadingOne>
                 <div className={styles.deliveryAvailableContainer}>
-                    <a href=''><img className={styles.deliveryAvailableSectionDD} src={DoorDashLogo} alt='Door Dash Logo'/></a>
-                    <img className={styles.deliveryAvailableSectionUE} src={UberEatsLogo} alt='Uber Eats Logo'/>
-                    <img className={styles.deliveryAvailableSectionDD} src={GrubhubLogo} alt='Grubhub Logo'/>
+                    <a href='https://www.doordash.com/store/senoritas-mexican-restaurant-(van-dyke-ave)-shelby-township-23427402/' target='blank'><img className={styles.deliveryAvailableSectionDD} src={DoorDashLogo} alt='Door Dash Logo'/></a>
+                    <a href='https://www.ubereats.com/store/senoritas-mexican-restaurant/J_oL6AhYVxyJt8DqLxLkBA' target='blank'><img className={styles.deliveryAvailableSectionUE} src={UberEatsLogo} alt='Uber Eats Logo'/></a>
+                    <a href='https://www.grubhub.com/restaurant/senoritas-mexican-restaurant-48758-van-dyke-ave-shelby-township/3301036' target='blank'><img className={styles.deliveryAvailableSectionDD} src={GrubhubLogo} alt='Grubhub Logo'/></a>
                 </div>
             </div>
         </div>
