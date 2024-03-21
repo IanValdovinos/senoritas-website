0be657f0bb73f879961bb90e089868ebd15c4881
Ian Samuel Valdovinos Granados <ian.valdovinos752001@gmail.com>
2024-03-20 20:23:11 -0400

    Collapse mobile navigation bar upon page selection

--- src/components/Navbar/NavBarMobile.js
+++ src/components/Navbar/NavBarMobile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from "react-router-dom";
 import styles from './NavBarMobile.module.css';
 
@@ -7,45 +7,51 @@ import Logo from '../../assets/images/logo_transparent.png';
 import Hamburger from '../../assets/images/icons/white_hamburger_icon.png';
 
 function NavBarMobile() {
+
+    // Collapse the navigation bar upon page selection
+    const handleClick = () => {
+        var linkContainer = document.getElementById('links');
+        linkContainer.style.display = "none";
+    };
+
+    // Opens and closes the navbar when the hamburger icon is pressed
     const onHamburgerClick = () => {
-        var x = document.getElementById('links');
+        var linkContainer = document.getElementById('links');
         
-        if (x.style.display === "block") {
-          x.style.display = "none";
+        if (linkContainer.style.display === "block") {
+          linkContainer.style.display = "none";
         } else {
-          x.style.display = "block";
+          linkContainer.style.display = "block";
         }
     }
 
     return ( 
         <div>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-
             <div className={styles.topnav}>
                 <img className={styles.navbarLogo} src={Logo}  alt="Restaurant logo"/>
 
                 <div className={styles.myLinks} id='links'>
-                    <NavLink className={styles.navbarLink} to="/">
+                    <NavLink onClick={handleClick} className={styles.navbarLink} to="/">
                         Home
                     </NavLink>
 
-                    <NavLink className={styles.navbarLink} to="/about">
+                    <NavLink onClick={handleClick} className={styles.navbarLink} to="/about">
                         About
                     </NavLink>
 
-                    <NavLink className={styles.navbarLink} to="/">
+                    <NavLink onClick={handleClick} className={styles.navbarLink} to="/">
                         Menu
                     </NavLink>
 
-                    <NavLink className={styles.navbarLink} to="/">
+                    <NavLink onClick={handleClick} className={styles.navbarLink} to="/">
                         Catering
                     </NavLink>
 
-                    <NavLink className={styles.navbarLink} to="/about">
+                    <NavLink onClick={handleClick} className={styles.navbarLink} to="/about">
                         Contact
                     </NavLink>
 
-                    <NavLink className={styles.navbarLink} to="/about">
+                    <NavLink onClick={handleClick} className={styles.navbarLink} to="/about">
                         Shop
                     </NavLink>
                 </div>
